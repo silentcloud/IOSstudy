6879d003dcdb695bb2aa618e78af3b5fe325037f
竹卢 <qiang.mou@alipay.com>
2013-10-30 22:39:53 +0800

    Initial Commit

---
+++ TabNav/TabNavAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TabNavAppDelegate.h
+//  TabNav
+//
+//  Created by silentcloud on 10/30/13.
+//  Copyright (c) 2013 qiang.mou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TabNavAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TabNav/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TabNav
+//
+//  Created by silentcloud on 10/30/13.
+//  Copyright (c) 2013 qiang.mou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TabNavAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TabNavAppDelegate class]));
+    }
+}
